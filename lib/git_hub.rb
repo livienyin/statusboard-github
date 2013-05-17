d0042ce6974ab452e0dbbc6b250a9e65c7ae3e68
Livien Yin <LivienYin@gmail.com>
2013-05-16 23:06:38 -0700

    Creates class for GitHubAPIClient and describes spec

---
+++ lib/git_hub.rb
@@ -0,0 +1,16 @@
+require 'net/http'
+
+module GitHub
+  class GitHubAPIClient
+    @@base_url = something
+    
+    def make_request(uri)
+      
+    end
+    
+  end
+  
+  class GitHubRepoFetcher
+    
+  end
+end
