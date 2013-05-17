d4454e79570e1575e21dc16d56403b9c6706826d
Livien Yin <LivienYin@gmail.com>
2013-05-16 23:06:22 -0700

    Creates class for GitHubAPIClient and describes spec

---
+++ spec/api_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+describe GitHub::GitHubAPIClient do
+  let(:api_client) do
+    GitHub::GitHubAPIClient.new username
+  end
+
+  let(:username) do
+    "livienyin"
+  end
+  
+  it "makes a uri request" do
+   
+  end
+  
+end
+
