c3c901c326035f692f7d55ebce693b02f3c58c06
Livien Yin <LivienYin@gmail.com>
2013-05-16 22:49:15 -0700

    Initial commit

---
+++ app/controllers/graph_controller.rb
@@ -0,0 +1,7 @@
+class GraphController < ApplicationController
+  
+  def statusboard
+    render :json => {}
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StatusboardGithub::Application.config.secret_token = '0bab1c1e50ade87e830ebc32b8271939af1532c22c6544c47539ea83844db92afe21bdd20bad0bc8fafbef2e0e53060a9757e88b268c343eba2759010807af79'
