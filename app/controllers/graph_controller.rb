ea3d1fc8707e79dc1cfdefc702cb87a1f43659fb
Livien Yin <LivienYin@gmail.com>
2013-05-17 00:51:44 -0700

    Makes GitHub API call to retrieve user repos and commits per repo

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -1,7 +1,27 @@
 class GraphController < ApplicationController
   
   def statusboard
-    render :json => {}
+    octo = Octokit::Client.new
+    repos = octo.repositories "livienyin"
+    data = []
+    repos.each do
+      |repo| data << {
+        "name" => repo.name,
+        "value" => octo.commits({
+          :user => "livienyin", :name => repo.name
+        }).length
+      }
+    end
+    statusboard = {
+      'title' => 'Git Hub: Commits By Repo',
+      'datasequences' => [
+        {
+          'title' => 'Commits By Repo',
+          'datapoints' => data
+        }
+      ]
+    }
+    render :json => {graph: statusboard}
   end
 
 end
