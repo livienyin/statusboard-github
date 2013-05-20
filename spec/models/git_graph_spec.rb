4d7fba9d8e0b1abee86694e575b0404d99253235
Livien Yin <LivienYin@gmail.com>
2013-05-20 10:05:58 -0700

    Wrote test messages to be more consistent across controller and model specs

--- spec/models/git_graph_spec.rb
+++ spec/models/git_graph_spec.rb
@@ -5,14 +5,14 @@ describe GitGraph do
   before do
     @fake_client = double(Octokit::Client)
     @fake_client.stub!(:repositories).and_return([double(:name => "repo_name")])
-    @fake_client.stub!(:commits).and_return(["These are supposed to be commit information things", "this shit does not matter"])
+    @fake_client.stub!(:commits).and_return(["These are supposed to be commit information things", "this content does not matter"])
     Octokit::Client.stub(:new).and_return(@fake_client)
-    @username = "Livien"
+    @username = "livienyin"
   end
 
   it "uses initialized username" do
     gitgraph = GitGraph.new(@username)
-    expect(gitgraph.username).to eq "Livien"
+    expect(gitgraph.username).to eq "livienyin"
     @fake_client.should_receive(:commits).with({:user => @username, :name => "repo_name"})
     @fake_client.should_receive(:repositories).with(@username)
     data = gitgraph.fetch_repo_commits
